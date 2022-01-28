6729e920e904bc7d0c4655bc40e6f89bbfafff1d
Luciano Roccasalvo <lucho.r9@live.com.ar>
2022-01-28 20:21:14 -0300

    agrego nueva pag java

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,47 @@
 let nombre = "";
 let edad = Number;
 let certificado = "si";
-let listaNombres = [];
 
-while (nombre == "" || nombre == " ") {
-    nombre = prompt ("Ingresar nombre");
-    listaNombres.push(nombre);
+
+    function pedirNombre (){
     
-    if (nombre != "" && nombre != " "){
-        alert ("Bienvenido " + nombre);
+        let listaNombres = [];
+
+        while (nombre == "" || nombre == " ") {
+        nombre = prompt ("Ingresar nombre");
+        listaNombres.push(nombre);
+    
+        if (nombre != "" && nombre != " "){
+        alert ("Bienvenido " + nombre);   
+        }   
+
+        }
+        return nombre;
+    }
+
+    
+    function pedirEdad (){
+
         edad = prompt ("Ingresar edad");
-    }
-}
-
-    if (edad <= 17){
+        if (edad <= 17){
         alert (nombre + " necesitas autorizacion de tus padres");
-    }else if (edad > 17){
-        certificado = prompt (nombre + " tiene certificado medico?");
+        }
+        else if (edad > 17){
+            alert (nombre + " para entrenar necesitas certificado medico");
+            certificado = prompt("Tenes certificado medico?");
+            if (certificado == "si"){
+                alert (nombre + " podes ingresar al gym");
+            }else{
+                alert ("Necesitas certificado medico " + nombre);
+            }
+        
+        }
+
+        return edad;
     }
 
-    if (certificado == "si"){
-        alert (nombre + " podes ingresar al gym");
-    }else{
-        alert (nombre + " necesitas certificado medico");
-    }
+    
+
+
+    pedirNombre(nombre)
+    pedirEdad(edad)
