858d64a8531bdc6140d67bffeaf4bd64f599cfdf
Luciano Roccasalvo <lucho.r9@live.com.ar>
2022-02-02 21:27:32 -0300

    modificacion java

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,24 @@
-let nombre = "";
-let edad = Number;
-let certificado = "si";
+        let nombre = document.getElementById('#nombre');
+            if (nombre != null) {
+                nombre.value;
+            }
 
-
-    function pedirNombre (){
-    
-        let listaNombres = [];
-
-        while (nombre == "" || nombre == " ") {
-        nombre = prompt ("Ingresar nombre");
-        listaNombres.push(nombre);
-    
-        if (nombre != "" && nombre != " "){
-        alert ("Bienvenido " + nombre);   
-        }   
-
-        }
-        return nombre;
-    }
-
-    
-    function pedirEdad (){
-
-        edad = prompt ("Ingresar edad");
-        if (edad <= 17){
-        alert (nombre + " necesitas autorizacion de tus padres");
-        }
-        else if (edad > 17){
-            alert (nombre + " para entrenar necesitas certificado medico");
-            certificado = prompt("Tenes certificado medico?");
-            if (certificado == "si"){
-                alert (nombre + " podes ingresar al gym");
-            }else{
-                alert ("Necesitas certificado medico " + nombre);
+        let apellido = document.getElementById('#apellido');
+            if (apellido != null) {
+                apellido.value
             }
         
-        }
+        let edad = document.getElementById('#edad');
+            if (edad != null) {
+            edad.value
+            }
 
-        return edad;
-    }
+        let email = document.getElementById('#email');
+            if (email != null) {
+            email.value
+            }
 
-    
-
-
-    pedirNombre(nombre)
-    pedirEdad(edad)
+        let certificado = document.getElementById('#certificado');
+            if (certificado != null) {
+                certificado.value
+            }
