725216f0b97249ec9194d7c2bcb68e2e1f93af1f
Luciano Roccasalvo <lucho.r9@live.com.ar>
2022-02-22 17:43:21 -0300

    agregando sweet

---
+++ scripts/horarios.js
@@ -0,0 +1,77 @@
+function hanldeClickRunning() {
+
+    Swal.fire({
+        title: 'Lunes, Miercoles y Viernes',
+        text: '8:00 - 10:00 - 12:00 - 14:00 - 16:00 - 18:00 - 20:00',
+        imageUrl: '../imagenes/Olympos.png',
+        imageWidth: 200,
+        imageHeight: 100,
+        imageAlt: 'Custom image',
+    })
+
+}
+
+function hanldeClickBody() {
+
+    Swal.fire({
+        title: 'Lunes y Jueves',
+        text: '8:30 - 10:30 - 12:30 - 14:30 - 16:30 - 18:30 - 20:30',
+        imageUrl: '../imagenes/Olympos.png',
+        imageWidth: 200,
+        imageHeight: 100,
+        imageAlt: 'Custom image',
+    })
+    
+}
+
+function hanldeClickHiit() {
+
+    Swal.fire({
+        title: 'Lunes y Jueves',
+        text: '8:00 - 10:00 - 12:00 - 14:00 - 16:00 - 18:00 - 20:00',
+        imageUrl: '../imagenes/Olympos.png',
+        imageWidth: 200,
+        imageHeight: 100,
+        imageAlt: 'Custom image',
+    })
+    
+}
+
+function hanldeClickZumba() {
+
+    Swal.fire({
+        title: 'Lunes, Martes, Miercoles, Jueves y Viernes',
+        text: '14:00 - 16:00 - 18:00 - 20:00',
+        imageUrl: '../imagenes/Olympos.png',
+        imageWidth: 200,
+        imageHeight: 100,
+        imageAlt: 'Custom image',
+    })
+    
+}
+
+function hanldeClickIndoor() {
+
+    Swal.fire({
+        title: 'Lunes, Martes, Miercoles, Jueves y Viernes',
+        text: '14:00 - 16:00 - 18:00 - 20:00',
+        imageUrl: '../imagenes/Olympos.png',
+        imageWidth: 200,
+        imageHeight: 100,
+        imageAlt: 'Custom image',
+    })
+    
+}
+
+function hanldeClickFight() {
+
+    Swal.fire({
+        title: 'Martes, Jueves y Sabado',
+        text: '14:00 - 16:00 - 18:00 - 20:00',
+        imageUrl: '../imagenes/Olympos.png',
+        imageWidth: 200,
+        imageHeight: 100,
+        imageAlt: 'Custom image',
+    })
+    
+}
